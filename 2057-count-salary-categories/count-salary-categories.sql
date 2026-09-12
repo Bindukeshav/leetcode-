7ba21f924719da481f3ea3408754e2d59f7f98b6
BinduKeshav <162860726+Bindukeshav@users.noreply.github.com>
2026-09-12 19:50:20 +0530

    Time: 1601 ms (56.06%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -2,17 +2,17 @@
 select 
     "Low Salary" AS category,
      COUNT(income)AS accounts_count
-FROM accounts 
+FROM Accounts 
 where income < 20000
 UNION
 SELECT 
     "Average Salary" AS category,
     COUNT(income)as accounts_count 
-FROM accounts
-WHERE income >= 20000 AND INCOME <= 50000
+FROM Accounts
+WHERE INCOME >= 20000 AND INCOME <= 50000
 UNION
 SELECT 
      "High Salary" AS category,
      COUNT(income)as accounts_count 
-FROM accounts 
-WHERE income > 50000
+FROM Accounts 
+WHERE INCOME > 50000
